baf90dba86a5d988e5737939b5083378ce7d4e86
Alfredo Medrano Sánchez <alfredomedranosanchez@gmail.com>
2020-11-13 15:11:35 -0600

    Feature: add new build settings

--- service/src/Services/API.js
+++ service/src/Services/API.js
@@ -1,13 +1,13 @@
 import { create } from 'apisauce';
 import { store } from '../Redux/store';
-import { dev, URL_DEV, URL_TEST } from '../Settings/general.json';
+import { getServer } from '../Utils/url';
 
 export const token = () => store.getState().auth.token;
 // Define the api
 const API = create({
-  baseURL: dev ? URL_DEV : URL_TEST,
+  baseURL: getServer(),
 });
 
 export async function login(email, password) {
-  return await API.post('/signin', { email, password });
+  return await API.post('/auth', { email, password });
 }
